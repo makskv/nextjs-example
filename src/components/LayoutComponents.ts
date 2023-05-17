aade533cb2c4dcfcac2397d65ea2e138c95bb3fb
Maksym Kovalenko <maksym.k@etheric.agency>
2023-05-17 19:22:51 +0200

    feat: implement styles, add fonts

---
+++ src/components/LayoutComponents.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const Label = styled.span`
+  font-size: 14px;
+  line-height: 16px;
+`
+
+export const Subtitle = styled.span`
+  color: var(--color-dark-grey);
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 1;
+`
+
+export const HeadlineS = styled.h4`
+	color: var(--color-dark-blue);
+	font-size: 16px;
+	line-height: 1;
+	letter-spacing: 0;
+  text-overflow: ellipsis;
+	margin: 0;
+  font-weight: 700;
+`
+
+export const HeadlineM = styled.h4`
+	color: var(--color-dark-blue);
+	font-size: 24px;
+	line-height: 1;
+	letter-spacing: 0;
+  text-overflow: ellipsis;
+	margin: 0;
+  font-weight: 700;
+`
+
+export const LabelAccent = styled.span`
+	font-size: 12px;
+	text-transform: uppercase;
+	line-height: 16px;
+  font-weight: 700;
+	color: var(--color-orange);
+`
+
+export const LabelBodyM = styled.span`
+	font-size: 16px;
+	line-height: 1;
+	color: var(--color-dark-blue);
+`
+
+export const LabelBodyS = styled.span`
+	font-size: 12px;
+	line-height: 14px;
+	color: var(--color-dark-grey);
+`
